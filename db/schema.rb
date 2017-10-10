a0ae070b40e370c726d27ad5db3d93a4a2d7d670
jodyvanden <jody.vanden@gmail.com>
2017-10-10 11:05:42 +1100

    add table comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010000049) do
+ActiveRecord::Schema.define(version: 20171010000518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "video_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["video_id"], name: "index_comments_on_video_id"
+  end
 
   create_table "marks", force: :cascade do |t|
     t.integer "mark"
@@ -50,6 +60,8 @@ ActiveRecord::Schema.define(version: 20171010000049) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "videos"
   add_foreign_key "marks", "users"
   add_foreign_key "marks", "videos"
 end
