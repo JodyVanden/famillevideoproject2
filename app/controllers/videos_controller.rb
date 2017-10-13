330a214b761a7a1533491f216e86dfa44060d401
jodyvanden <jody.vanden@gmail.com>
2017-10-13 12:00:03 +1100

    modify video_controller, the show and index of video to add the youtube_id

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,12 +8,19 @@ class VideosController < ApplicationController
         pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
         match_data = video.url.match(pattern)
         video.youtube_id = match_data[:youtube_id]
+        video.save!
       end
     end
   end
 
   def show
     @video = Video.find(params[:id])
+    if @video.youtube_id.nil?
+        pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
+        match_data = @video.url.match(pattern)
+        @video.youtube_id = match_data[:youtube_id]
+        @video.save
+      end
     @content = Comment.new
     @mark = Mark.new
   end
