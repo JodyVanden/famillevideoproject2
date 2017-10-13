15973e411478b0c250cd61169adefcb9099ebcdb
jodyvanden <jody.vanden@gmail.com>
2017-10-13 11:49:08 +1100

    add fonction on index to fill up youtube_id if this is empty

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,17 +2,18 @@ class VideosController < ApplicationController
 
   def index
     @videos = Video.all
+
+    @videos.each do |video|
+      if video.youtube_id.nil?
+        pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
+        match_data = video.url.match(pattern)
+        video.youtube_id = match_data[:youtube_id]
+      end
+    end
   end
 
   def show
     @video = Video.find(params[:id])
-
-    if @video.youtube_id.nil?
-      pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
-      match_data = @video.url.match(pattern)
-      @video.youtube_id = match_data[:youtube_id]
-    end
-
     @content = Comment.new
     @mark = Mark.new
   end
