064a0d9889a39403dcfb7d0e4f38743487c1b5c3
jodyvanden <jody.vanden@gmail.com>
2017-10-13 11:39:49 +1100

    add column youtub_id to video table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010020930) do
+ActiveRecord::Schema.define(version: 20171013003905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20171010020930) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "youtube_id"
   end
 
   add_foreign_key "comments", "users"
