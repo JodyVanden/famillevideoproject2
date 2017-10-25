a34bd847efc5f85152cdeddc5867e8af4b98fccc
jodyvanden <jody.vanden@gmail.com>
2017-10-25 12:30:02 +1100

    add heroku

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,7 +12,6 @@ class VideosController < ApplicationController
         pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
         match_data = video.url.match(pattern)
         video.youtube_id = match_data[:youtube_id]
-
       end
         video.save
     end
