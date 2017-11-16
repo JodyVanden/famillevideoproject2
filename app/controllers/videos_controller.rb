db93a7a55e2ddc41aeca3460112cbc20f5a47acb
jodyvanden <jody.vanden@gmail.com>
2017-11-16 22:04:30 +1100

    modify the controller to add the video information for the image coming from youtube

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,8 +12,9 @@ class VideosController < ApplicationController
          # pattern = /(?:https\:\/\/youtu\.be)\/(?<youtube_id>.+)/
         # match_data = @video.url.match(pattern)
         # @video.youtube_id = match_data[:youtube_id]
-        video_youtube_info = VideoInfo.new(@video.url)
-        @video.youtube_id = video_youtube_info.video_id
+        video_youtube_info = VideoInfo.new(video.url)
+        video.youtube_id = video_youtube_info.video_id
+        video.image_url = video_youtube_info.thumbnail_large
       end
         video.save
     end
