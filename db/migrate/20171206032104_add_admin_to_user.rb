dba9dfe04fe8e017300be8fe8fe7b38894b4c95b
jodyvanden <jody.vanden@gmail.com>
2017-12-06 14:49:34 +1100

    add field admin and family to users to be able to filter the results

---
+++ db/migrate/20171206032104_add_admin_to_user.rb
@@ -0,0 +1,6 @@
+class AddAdminToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :admin, :boolean, null: false, default: false
+    add_column :users, :family, :string, default: "none"
+  end
+end
